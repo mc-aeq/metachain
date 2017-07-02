d067c01ef33a24f4e5c2fe1153b4aa8b86336ab5
FireMail <FireMail@trustchaintechnologies.io>
2017-07-02 11:00:37 +0200

    platform independent build setup

---
+++ MetaChain.cpp
@@ -0,0 +1,24 @@
+/*
+	start point for the metachain
+*/
+#include "stdafx.h"
+#ifdef _WINDOWS
+	
+#endif
+
+#ifdef _LINUX
+	
+#endif
+
+using namespace std;
+
+// main entry point
+int main( int argc, char* argv[] )
+{
+	cout << "base start for a platform independent setup" << endl;
+	cin.get();
+
+	// end metachain
+    return 0;
+}
+
